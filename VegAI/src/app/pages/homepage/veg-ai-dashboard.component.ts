4bf09436531b2db35615fc8e48ca0f57cc0181d3
Stephanope <stefansucatu@gmail.com>
2025-02-08 17:49:43 -0500

    fix

--- VegAI/src/app/pages/homepage/veg-ai-dashboard.component.ts
+++ VegAI/src/app/pages/homepage/veg-ai-dashboard.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-veg-ai-dashboard',
   templateUrl: './veg-ai-dashboard.component.html',
-  styleUrls: ['./veg-ai-dashboard.component.scss']
+  styleUrls: ['./veg-ai-dashboard.component.scss'],
+  standalone: true,
+  imports: [CommonModule, HeaderComponent, FormsModule]
 })
+
 export class VegAiDashboardComponent {
   userName = 'User';
 
