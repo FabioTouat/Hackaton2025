4ed55589cfce193dd1d1806a5f53f5d5f7a4ea7a
Fabio Laberge-Touat <Fabio.l.touat@gmail.com>
2025-02-09 05:18:10 -0500

    poulate avais disparue

--- VegAI/server/src/server.ts
+++ VegAI/server/src/server.ts
@@ -4,21 +4,25 @@ import dotenv from 'dotenv';
 import connectDB from './config/database';
 import authRoutes from './routes/auth';
 import aiRoutes from './routes/ai';
-
+import vegetablesRoutes from './routes/vegetables';
+import { populateVegetables } from './models/vegetable';
 dotenv.config();
+
+
 
 const app = express();
 
 // Connect to MongoDB
 connectDB();
+populateVegetables();
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/ai', aiRoutes);
-
+app.use('/api/vegetables', vegetablesRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
